f6d7a4b735e28055b5d998c470953c39a2159b5c
cathywang0424 <62360925+cathywang0424@users.noreply.github.com>
2020-04-16 15:13:22 -0700

    Update Final Project Team05.py

--- Final Project Team05.py
+++ Final Project Team05.py
@@ -3,12 +3,14 @@
 
 # take input from user (height, weight, sex*)
 # translate these values into a BMI values (math function)
-# translate the BMI value into the ideal daily calorie intake (math function)
-# design a one day diet according to the calorie(including breakfast, lunch, dinner)
-# ask the user if he or she is vegan
+# translate the BMI value into the ideal daily calorie intake， preferably a range (math function)
+# design a one day diet according to the calorie range (including breakfast, lunch, dinner)
+    # we can design a total of 20 diets for example, each have a different calorie content. then, we can randomly choose one that is within the range of the calorie intake
+    # for instance, if the ideal calorie intake is from 1500-2000, the function will randomly return diets within that range
+# ask the user if he or she is vegan(?)
 # if yes, return a vegan diet
 # if no, return a normal diet
-# ask the user if he or she wants a snack (if yes, return a snack that is within the calculated ideal calorie intake)
+# ask the user if he or she wants a snack (if yes, return a snack)
 # print out the diet in a pretty format
 # ask if the user want another day's diet
 # if yes, circle back
