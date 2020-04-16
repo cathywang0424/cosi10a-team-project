d1d4376e13b9a0cd789cdd10145ff422a542c79a
cathywang0424 <62360925+cathywang0424@users.noreply.github.com>
2020-04-16 15:06:45 -0700

    Update Final Project Team05.py

--- Final Project Team05.py
+++ Final Project Team05.py
@@ -1,2 +1,15 @@
-# This is a calorie table (pseudocode)
-print("Hahahaha")
+""" Final Group Project"""
+# This is a calorie calculator.
+
+# take input from user (height, weight)
+# translate these values into a BMI values (math function)
+# translate the BMI value into the ideal daily calorie intake (math function)
+# design a one day diet according to the calorie(including breakfast, lunch, dinner)
+# ask the user if he or she is vegan
+# if yes, return a vegan diet
+# if no, return a normal diet
+# ask the user if he or she wants a snack (if yes, return a snack that is within the calculated ideal calorie intake)
+# print out the diet in a pretty format
+# ask if the user want another day's diet
+# if yes, circle back
+# if not, exit the program
